779242ff4885f549078569a8efce0f870ba185bb
aditya-wrmn <aditya_wrmn@protonmail.com>
2020-11-11 17:28:01 +0700

    functional, read command and write temporary file

--- goText.go
+++ goText.go
@@ -1,21 +1,134 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"log"
+	"io"
 	"os"
+
+	"github.com/tcnksm/go-input"
 )
 
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return (err != nil)
+}
+
+func rw(target string, argu int) error {
+	file, err := os.OpenFile(target, os.O_RDWR, 0644)
+
+	if isError(err) {
+		return nil
+	} else {
+		fmt.Println("Opening ", target)
+		fmt.Printf("================\n\n")
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	switch argu {
+	case 1:
+		write(scanner, target)
+	case 2:
+		read(scanner, target)
+	}
+
+	defer file.Close()
+
+	return nil
+}
+
+func newFile(file string) error {
+	return nil
+}
+
+func write(file *bufio.Scanner, target string) error {
+	var idx []string
+
+	f, err := os.Create(target + ".temp")
+
+	for file.Scan() {
+		idx = append(idx, edit(file.Text()))
+	}
+	for _, v := range idx {
+		fmt.Fprintln(f, v)
+		if isError(err) {
+			return err
+		}
+	}
+	return nil
+}
+
+func read(text *bufio.Scanner, target string) error {
+
+	for text.Scan() {
+		fmt.Println(text.Text())
+	}
+
+	if isError(text.Err()) {
+		return nil
+	}
+
+	return nil
+
+}
+
+func deleteFile(file string) error {
+	return nil
+}
+
+func edit(line string) string {
+	ui := &input.UI{
+		Writer: os.Stdout,
+		Reader: os.Stdin,
+	}
+
+	afterEdit, err := ui.Ask("", &input.Options{
+		Default: ": " + line,
+	})
+
+	if isError(err) {
+		return ""
+	}
+
+	return afterEdit
+}
+
+func Copy(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+	return out.Close()
+}
+
 func main() {
-	openFile := os.Args
-	file, err := os.Open(openFile[1])
-	if err != nil {
-		log.Fatal(err)
+	command := os.Args
+
+	switch command[1] {
+	case "new":
+		newFile(command[2])
+	case "write":
+		rw(command[2], 1)
+	case "read":
+		rw(command[2], 2)
+	case "delete":
+		deleteFile(command[2])
+	default:
+		fmt.Print("Invalid command")
 	}
-	data := make([]byte, 100)
-	count, err := file.Read(data)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%q\n", data[:count])
 }
